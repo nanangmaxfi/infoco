70e1b249b586faa52509990e70aa02e80a4fb84b
Nanang Maxfi <n.maxfi.22@gmail.com>
2020-04-03 08:24:47 +0700

    init

---
+++ app/src/main/java/id/web/nanangmaxfi/infoco/ui/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package id.web.nanangmaxfi.infoco.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.web.nanangmaxfi.infoco.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/id/web/nanangmaxfi/infoco/ui/province/ProvinceActivity.java
@@ -0,0 +1,16 @@
+package id.web.nanangmaxfi.infoco.ui.province;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.web.nanangmaxfi.infoco.R;
+
+public class ProvinceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_province);
+    }
+}
